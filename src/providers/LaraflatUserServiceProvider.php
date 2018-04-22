b842c406a077ba5dc1264ece4bc0082a6ba1d8c3
Abdel Aziz Hassan <=>
2018-04-22 23:35:24 +0200

    Test Package

---
+++ src/providers/LaraflatUserServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Laraflat\Users\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaraflatUserServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        dd('ssddsds');
+    }
+}
